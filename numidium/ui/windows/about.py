37fc07bad4a14786905e92d0d25196ebc1c1f794
OperatorJack <thepaladinof@gmail.com>
2022-01-22 13:35:55 -0500

    Attempt to improve About window with some actual information / source code link.

--- numidium/ui/windows/about.py
+++ numidium/ui/windows/about.py
@@ -1,16 +1,26 @@
-from PySide6.QtWidgets import QLabel, QVBoxLayout, QWidget
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QVBoxLayout, QWidget
+
+from numidium.ui.widgets import OpenGithubButton, TextBlockLabel
 
 
 class AboutWindow(QWidget):
+    label_description: TextBlockLabel
+    button_open_source_code: OpenGithubButton
+
     def __init__(self) -> None:
         super().__init__()
 
-        self.label = QLabel("About")
-        self.description = QLabel("This is a placeholder for our about information.")
+        self.label_description = TextBlockLabel(
+            "Numidium is an open source mod management tool for The Elder Scrolls III: Morrowind. The application is extensible through a modular plugin system and is inteded to be a singular tool for working with TESIII installations and plugins. For more information, use the button below to view the source code repository."
+        )
+        self.button_open_source_code = OpenGithubButton()
 
         layout = QVBoxLayout()
-        layout.addWidget(self.label)
-        layout.addWidget(self.description)
+        layout.setAlignment(Qt.AlignTop)
+        layout.addWidget(self.label_description)
+        layout.addWidget(self.button_open_source_code)
 
         self.setLayout(layout)
-        self.resize(200, 400)
+        self.setMaximumSize(500, 150)
+        self.setWindowTitle("About")
