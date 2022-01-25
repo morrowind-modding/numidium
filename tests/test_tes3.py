3ed919e7d5e904911bbceb4a9563877691dceac6
Greatness7 <Greatness7@gmail.com>
2022-01-24 21:04:37 -0500

    Move tests directory outside of application code.

--- tests/test_tes3.py
+++ tests/test_tes3.py
@@ -2,15 +2,15 @@ from numidium import tes3
 
 
 def test_load() -> None:
-    plugin = tes3.Plugin.load("numidium/tes3/tests/assets/OAAB_Data.esm")
+    plugin = tes3.Plugin.load("tests/assets/test.esp")
     header = plugin.objects[0]
 
     assert type(header) is tes3.Header
     assert header.flags1 == 0
     assert header.flags2 == 0
     assert header.version <= 1.3
-    assert header.file_type == tes3.FileType.Esm
+    assert header.file_type == tes3.FileType.Esp
     assert header.author == ""
     assert header.description == ""
-    assert header.num_objects == 3656
+    assert header.num_objects == 79
     assert header.masters == [("Morrowind.esm", 79837557), ("Tribunal.esm", 4565686), ("Bloodmoon.esm", 9631798)]
