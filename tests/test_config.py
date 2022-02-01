0a7b68c828177da80a4e801bca7633d677820bf4
Greatness7 <Greatness7@gmail.com>
2022-02-01 08:25:34 -0500

    Remove outdated tests.

--- tests/test_config.py
+++ tests/test_config.py
@@ -2,41 +2,19 @@ from io import StringIO
 from pathlib import Path
 from tempfile import TemporaryDirectory
 
-import pytest
-
 from numidium.config import Config
 
-test_case_args = ["json_str", "config"]
-test_vase_vals = [
-    ('{"active_extensions":[]}', Config(active_extensions=[])),
-    ('{"active_extensions":["a"]}', Config(active_extensions=["a"])),
-    ('{"active_extensions":["a","b"]}', Config(active_extensions=["a", "b"])),
-]
 
-
-@pytest.mark.parametrize(test_case_args, test_vase_vals)
-def test_load(json_str: str, config: Config) -> None:
-    c = Config()
-    c.load(StringIO(json_str))
-    assert c == config
-
-
-@pytest.mark.parametrize(test_case_args, test_vase_vals)
-def test_save(json_str: str, config: Config) -> None:
-    with StringIO() as f:
-        config.save(f)
-        assert f.getvalue() == json_str
-
-
-@pytest.mark.parametrize(test_case_args, test_vase_vals)
-def test_load_save(json_str: str, config: Config) -> None:
+def test_load_save() -> None:
     c = Config()
 
     with StringIO() as f:
         c.save(f)
         f.seek(0)
-        config.load(f)
-        assert c == config
+
+        c2 = Config()
+        c2.load(f)
+        assert c == c2
 
 
 def test_backup_created() -> None:
