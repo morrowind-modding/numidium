8a1e58347b7777812d9af7a54aa1052d7d69a967
Greatness7 <Greatness7@gmail.com>
2022-02-02 17:26:47 -0500

    Add (currently unused) file watcher signals.

--- numidium/ui/signals.py
+++ numidium/ui/signals.py
@@ -1,6 +1,7 @@
 from datetime import datetime
+from os.path import isdir
 
-from PySide6.QtCore import QObject, Signal
+from PySide6.QtCore import QFileSystemWatcher, QObject, Signal
 
 from numidium.config import config
 
@@ -8,23 +9,35 @@ from numidium.config import config
 class Signals(QObject):
     workspace_changed = Signal(str)
 
+    watcher: QFileSystemWatcher
+    directory_changed = Signal(str)
+    file_changed = Signal(str)
+
     def __init__(self) -> None:
         super().__init__()
 
         self.workspace_changed.connect(self.handle_workspace_changed)
 
+        self.watcher = QFileSystemWatcher(parent=self)
+        self.watcher.directoryChanged.connect(self.directory_changed.emit)
+        self.watcher.fileChanged.connect(self.file_changed.emit)
+
     def handle_workspace_changed(self, workspace: str) -> None:
-        if workspace != config.active_workspace:
-            config.active_workspace = workspace
-            config.recent_workspaces[workspace] = int(datetime.now().timestamp())
+        if isdir(workspace):
+            self.update_active_worskpace(workspace)
+            self.update_recent_workspaces(workspace)
 
-            # Limit to maximum of 5 recent workspaces. TODO: customizable
-            if len(config.recent_workspaces) > 5:
-                # sort by time
-                items = list(config.recent_workspaces.items())
-                items.sort(key=lambda x: x[1], reverse=True)
-                # take first 5
-                config.recent_workspaces.clear()
-                config.recent_workspaces.update(items[:5])
+    def update_active_worskpace(self, workspace: str) -> None:
+        # self.watcher.removePath(config.active_workspace)
+        # self.watcher.addPath(workspace)
+        config.active_workspace = workspace
 
-            config.save_path()
+    def update_recent_workspaces(self, workspace: str) -> None:
+        """Update and sort the list of recent workspaces."""
+        # TODO: move this to Config class
+        config.recent_workspaces[workspace] = int(datetime.now().timestamp())
+        items = list(config.recent_workspaces.items())
+        items.sort(key=lambda x: x[1], reverse=True)
+        config.recent_workspaces.clear()
+        config.recent_workspaces.update(items)
+        config.save_path()
